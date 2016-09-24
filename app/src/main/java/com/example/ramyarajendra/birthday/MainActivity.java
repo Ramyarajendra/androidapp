17ced1d985853b6175c28a99f9b690f0f21be785
ramya <smartramya96@gmail.com>
2016-09-24 18:46:03 +0530

    bday app1

--- app/src/main/java/com/example/ramyarajendra/birthday/MainActivity.java
+++ app/src/main/java/com/example/ramyarajendra/birthday/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.ramyarajendra.birthday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);}
+
+        public void clickbutton(View view){
+        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+        startActivity(intent);
     }
-}
+    }
+
